ab02810e9b17f6bffd8f1e57e36594a305b01e58
Ato Lie <atollie234@gmail.com>
2025-01-19 07:16:41 +0700

    Fix issue with CamelCaseMotion keymap doesn't work with motion ex: cw

--- nvim/lua/config/plugins/camelcasemotion.lua
+++ nvim/lua/config/plugins/camelcasemotion.lua
@@ -1,10 +1,13 @@
 return {
   'bkad/CamelCaseMotion',
   event = { 'BufReadPre', 'BufNewFile' },
-  keys = {
-    { 'w', '<Plug>CamelCaseMotion_w' },
-    { 'b', '<Plug>CamelCaseMotion_b' },
-    { 'e', '<Plug>CamelCaseMotion_e' },
-    { 'ge', '<Plug>CamelCaseMotion_ge' },
-  }
+  init = function()
+    vim.api.nvim_set_keymap('', 'w', '<Plug>CamelCaseMotion_w', {})
+    vim.api.nvim_set_keymap('', 'b', '<Plug>CamelCaseMotion_b', {})
+    vim.api.nvim_set_keymap('', 'e', '<Plug>CamelCaseMotion_e', {})
+    vim.api.nvim_set_keymap('', 'ge', '<Plug>CamelCaseMotion_ge', {})
+    vim.keymap.set({'x', 'o'}, 'iw', '<Plug>CamelCaseMotion_iw')
+    vim.keymap.set({'x', 'o'}, 'ib', '<Plug>CamelCaseMotion_ib')
+    vim.keymap.set({'x', 'o'}, 'ie', '<Plug>CamelCaseMotion_ie')
+  end
 }
